906f99d8987288720d0678cc46fd6a12f5fd0567
LMiranda <luan.miranda103@gmail.com>
2023-05-24 10:28:22 -0300

    Set Hooks for Auth

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { api } from '../Services/api'
 
 const AuthContext = createContext({});
@@ -12,8 +12,12 @@ function AuthProvider({ children }) {
         try {
             const response = await api.post('/sessions', { email, password })
             const { user, token } = response.data;
+
+            localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+            localStorage.setItem('@rocketnotes:token', token)
+
             
-            api.defaults.headers.authorization = `Bearer ${token}`
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
             setData({ user, token })
 
         } catch(error) {
@@ -23,11 +27,54 @@ function AuthProvider({ children }) {
                 alert("Não foi possivel entrar")
             }
         }
-
     }
 
+    function signOut() {
+        const token = localStorage.removeItem('@rocketnotes:token')
+        const user = localStorage.removeItem('@rocketnotes:user')
+
+        setData({});
+    }
+
+    async function updateProfile({ user }) {
+        try {
+            
+            await api.put("/users", user);
+            localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+
+            setData({ user, token: data.token })
+            alert("PERFIL ATUALIZADO")
+
+        } catch(error) {
+            if(error.response){
+                alert(error.response.data.response)
+            } else {
+                alert("Não foi possivel atualizar o perfil")
+            }
+        }
+    }
+
+    useEffect(()=>{
+        const token = localStorage.getItem('@rocketnotes:token')
+        const user = localStorage.getItem('@rocketnotes:user')
+
+        if(token && user ) {
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+     }, [])
+
     return (
-        <AuthContext.Provider value={{ signIn, user: data.user  }}>
+        <AuthContext.Provider value={{ 
+            signIn, 
+            signOut, 
+            updateProfile, 
+            user: data.user }}>
+
             {children}
         </AuthContext.Provider>
     )
